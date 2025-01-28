d04e759c82038ef4810b0a20099bc51864551303
Carlos Romero <cmromero.dev@gmail.com>
2025-01-28 00:58:39 -0600

    refactor: Improve MDX component styling with tracking and formatting

--- app/blog/components/mdx-components.tsx
+++ app/blog/components/mdx-components.tsx
@@ -4,31 +4,86 @@ import { useMDXComponent } from "next-mdx-remote/rsc"
 
 const components = {
   h1: ({ className, ...props }: React.HTMLAttributes<HTMLHeadingElement>) => (
-    <h1 className={cn("mt-2 scroll-m-20 text-4xl font-bold", className)} {...props} />
+    <h1 
+      className={cn(
+        "mt-2 scroll-m-20 text-4xl font-bold tracking-tight",
+        className
+      )} 
+      {...props} 
+    />
   ),
   h2: ({ className, ...props }: React.HTMLAttributes<HTMLHeadingElement>) => (
-    <h2 className={cn("mt-10 scroll-m-20 border-b pb-2 text-3xl font-semibold", className)} {...props} />
+    <h2 
+      className={cn(
+        "mt-10 scroll-m-20 border-b pb-2 text-3xl font-semibold tracking-tight first:mt-0",
+        className
+      )} 
+      {...props} 
+    />
   ),
   h3: ({ className, ...props }: React.HTMLAttributes<HTMLHeadingElement>) => (
-    <h3 className={cn("mt-8 scroll-m-20 text-2xl font-semibold", className)} {...props} />
+    <h3 
+      className={cn(
+        "mt-8 scroll-m-20 text-2xl font-semibold tracking-tight",
+        className
+      )} 
+      {...props} 
+    />
   ),
   p: ({ className, ...props }: React.HTMLAttributes<HTMLParagraphElement>) => (
-    <p className={cn("leading-7 [&:not(:first-child)]:mt-6", className)} {...props} />
+    <p 
+      className={cn(
+        "leading-7 [&:not(:first-child)]:mt-6",
+        className
+      )} 
+      {...props} 
+    />
   ),
   ul: ({ className, ...props }: React.HTMLAttributes<HTMLUListElement>) => (
-    <ul className={cn("my-6 ml-6 list-disc", className)} {...props} />
+    <ul 
+      className={cn(
+        "my-6 ml-6 list-disc",
+        className
+      )} 
+      {...props} 
+    />
   ),
   ol: ({ className, ...props }: React.HTMLAttributes<HTMLOListElement>) => (
-    <ol className={cn("my-6 ml-6 list-decimal", className)} {...props} />
+    <ol 
+      className={cn(
+        "my-6 ml-6 list-decimal",
+        className
+      )} 
+      {...props} 
+    />
   ),
   li: ({ className, ...props }: React.HTMLAttributes<HTMLLIElement>) => (
-    <li className={cn("mt-2", className)} {...props} />
+    <li 
+      className={cn(
+        "mt-2",
+        className
+      )} 
+      {...props} 
+    />
   ),
   blockquote: ({ className, ...props }: React.HTMLAttributes<HTMLQuoteElement>) => (
-    <blockquote className={cn("mt-6 border-l-2 pl-6 italic", className)} {...props} />
+    <blockquote 
+      className={cn(
+        "mt-6 border-l-2 pl-6 italic",
+        className
+      )} 
+      {...props} 
+    />
   ),
   img: ({ className, alt, ...props }: React.ImgHTMLAttributes<HTMLImageElement>) => (
-    <img className={cn("rounded-md border", className)} alt={alt} {...props} />
+    <img 
+      className={cn(
+        "rounded-md border",
+        className
+      )} 
+      alt={alt} 
+      {...props} 
+    />
   ),
   code: ({ className, ...props }: React.HTMLAttributes<HTMLElement>) => (
     <code
