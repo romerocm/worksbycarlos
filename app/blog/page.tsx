7ce6a8b6be530221c1815044a0ceec43f2cd4de3
Carlos Romero <cmromero.dev@gmail.com>
2025-01-27 22:03:16 -0600

    fix: Remove duplicate import of MDXRemote in blog page component

--- app/blog/page.tsx
+++ app/blog/page.tsx
@@ -1,7 +1,6 @@
 "use client"
 
 import { useState } from 'react'
-import { MDXRemote } from 'next-mdx-remote/rsc'
 import { motion } from 'framer-motion'
 import path from 'path'
 import fs from 'fs'
